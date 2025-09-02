57549c641bf9cd5b4af0b9b323fdfc2b2fb70014
Christophe <chris.link@hotmail.fr>
2025-09-02 21:51:14 +0200

    ContactForm_supabase_done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ContactForm from "./components/ContactForm";
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <ContactForm />
+    </>
+  );
+}
+export default App;
---
+++ src/lib/deeplClient.ts
@@ -0,0 +1,18 @@
+import Deepl from "deepl-node";
+import type { TargetLanguageCode } from "deepl-node"; // ✅ import de type seulement
+
+// Crée le client avec ta clé API DeepL
+export const deepl = new Deepl.Translator(import.meta.env.VITE_DEEPL_KEY);
+
+export async function translateText(
+  text: string,
+  targetLanguage: TargetLanguageCode // <- type correct ici
+) {
+  try {
+    const result = await deepl.translateText(text, null, targetLanguage);
+    return result.text;
+  } catch (error) {
+    console.error("Erreur traduction DeepL:", error);
+    return "";
+  }
+}
